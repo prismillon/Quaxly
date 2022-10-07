3906dde60c994c7a9c0710018fcc4d6c3ebdf6b6
PPOIRIER <pierreherve.poirier@ynov.com>
2022-10-07 12:48:06 +0200

    quack a maintenant une base de donnée et sait s'en servir

--- Quaxly.js
+++ Quaxly.js
@@ -1,19 +1,32 @@
-const { Client, GatewayIntentBits, REST, Routes } = require('discord.js');
+const { Client, GatewayIntentBits, EmbedBuilder, ActivityType } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 const token = require('./config.json');
-const { readFileSync } = require('fs');
+const fs = require('fs');
+
+function is_track_init(user_id, speed, item, track) {
+  if(bdd[user_id] != undefined && bdd[user_id][speed] != undefined && bdd[user_id][speed][item] != undefined && bdd[user_id][speed][item][track] != undefined){
+    return true;
+  }
+  return false;
+}
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  setInterval(() => {
+    client.user.setActivity(`${client.guilds.cache.size} servers`, { type: ActivityType.Watching })
+}, 60000);
 });
 
-const track_list = readFileSync("./tracklist.txt", 'utf-8').split(/\r?\n/);
+bdd = JSON.parse(fs.readFileSync('./bdd.json', 'utf8'));
+
+const track_list = fs.readFileSync("./tracklist.txt", 'utf-8').split(/\r?\n/);
 
 client.on('interactionCreate', async interaction => {
   if (!interaction.isChatInputCommand()) return;
 
   if (interaction.commandName === 'ping') {
-    await interaction.reply({content: 'Pong! took ' + client.ws.ping + ' ms', ephemeral: true});
+    const embed = new EmbedBuilder().setTitle('Pong!').setColor(0x47e0ff).setDescription('took ' + client.ws.ping + 'ms');
+    await interaction.reply({embeds: [embed], ephemeral: true});
   }
   if (interaction.commandName === 'select') {
     await interaction.reply({content: `selected ${interaction.options.get('colors').value} and ${interaction.options.get('numbers').value}`, ephemeral: true});
@@ -23,25 +36,59 @@ client.on('interactionCreate', async interaction => {
     track = interaction.options.get('track').value;
     for (let i = 0; i < track_list.length; i++) {
         if (track_list[i].toLowerCase() == track.toLowerCase()) {
-            track = track_list[i];
-            break;
+          track = track_list[i];
+          break;
         }
         if (i == track_list.length - 1) {
-            await interaction.reply({content: `Track not found`, ephemeral: true});
-            return;
+          const embed = new EmbedBuilder().setTitle('Error').setColor(0xff0000).setDescription(`could not found \`${track}\` in the track list`);
+          await interaction.reply({embeds: [embed], ephemeral: true});
+          return;
         }
     }
     const time = interaction.options.get('time').value;
-    const reg = new RegExp('^[0-9]:[0-9]{2}\\.[0-9]{3}$');
+    const reg = new RegExp('^[0-9]:[0-5][0-9]\\.[0-9]{3}$');
     if (!reg.test(time)) {
-        await interaction.reply({content: `Invalid time format (\`${time}\`), please use this format -> \`1:23.456\``, ephemeral: true});
+      const embed = new EmbedBuilder().setTitle(`Invalid time format`).setColor(0xff0000).setDescription(`recived \`${time}\` please use this format instead -> \`1:23.456\``);
+      await interaction.reply({embeds: [embed], ephemeral: true});
+      return;
+    }
+    item = 'NI';
+    if (interaction.options.get('item').value == 1) {
+      item = 'Shroom';
+    }
+    if (is_track_init(interaction.user.id, speed, item, track)) {
+      if (bdd[interaction.user.id][speed][item][track] < time) {
+        const embed = new EmbedBuilder().setTitle(`Error`).setColor(0xff0000).setDescription(`you already have \`${bdd[interaction.user.id][speed][item][track]}\` on this track`);
+        await interaction.reply({embeds: [embed], ephemeral: true});
         return;
+      }
+      bdd[interaction.user.id][speed][item][track] = time;
     }
-    shroom = 'NI';
-    if (interaction.options.get('shroom').value == 1) {
-        shroom = 'Shroom';
+    if (bdd[interaction.user.id] == undefined) {
+      bdd[interaction.user.id] = {
+        "150": {
+          "Shroom": {
+          },
+          "NI": {
+          }
+        },
+        "200": {
+          "Shroom": {
+          },
+          "NI": {
+          }
+        }
+      };
     }
-    await interaction.reply({content: `Saved **${time}** on \`${track}\` in ${speed}cc (${shroom}) !`});
+    bdd[interaction.user.id][speed][item][track] = time;
+    fs.writeFile('./bdd.json', JSON.stringify(bdd, null, 4), (err) => {
+      if (err) {
+        interaction.reply({content: `error while saving the time`, ephemeral: true});
+        return;
+      }
+    })
+    const embed = new EmbedBuilder().setTitle(`Time saved`).setColor(0x47e0ff).setDescription(`saved **${time}** on \`${track}\`, ${speed}cc (${item})`);
+    await interaction.reply({embeds: [embed]});``
   }
 });
 
