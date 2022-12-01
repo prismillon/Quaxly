04f4a1172c02fc12d25e4ce0abc8c79382736681
prsm <71454541+prismillon@users.noreply.github.com>
2022-12-01 17:07:11 +0100

    all time

--- Quaxly.js
+++ Quaxly.js
@@ -60,7 +60,7 @@ fs.writeFileSync("./bdd_backup.json", JSON.stringify(bdd, null, 4));
 
 fs.writeFileSync("./user_list_backup.json", JSON.stringify(user_list, null, 4));
 
-console.log(fs.readFileSync("./tracklist.txt", "utf-8"));
+console.log(fs.readFileSync("./tracklist.txt", "utf-8").split(/\r?\n/));
 const track_list = fs.readFileSync("./tracklist.txt", "utf-8").split(/\r?\n/);
 
 client.on("interactionCreate", async (interaction) => {
