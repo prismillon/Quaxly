e120ca618fcec3cc71845a836d6e5918b9bb47da
PPOIRIER <pierreherve.poirier@ynov.com>
2023-07-12 23:51:50 +0200

    some more lineup fix

--- bot.py
+++ bot.py
@@ -29,6 +29,7 @@ async def on_ready():
     if not bot.api_list.is_running():
         bot.api_list.start()
 
+    await bot.wait_until_ready()
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{len(bot.guilds)} servers"))
 
 @bot.event
