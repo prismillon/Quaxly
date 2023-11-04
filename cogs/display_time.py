9270298729b58605112a7734231de79bdd71addd
prsm <71454541+prismillon@users.noreply.github.com>
2023-11-04 23:19:30 +0100

    display time now has categories to handle the number of tracks in a single embed better and name_history is now defered

--- cogs/display_time.py
+++ cogs/display_time.py
@@ -44,55 +44,78 @@ async def display_time(interaction: discord.Interaction, speed: Choice[str], ite
         embed.description = f"{player.display_name} is not registered in the server"
         return await interaction.response.send_message(embed=embed)
 
-    if player and not track:
-        total = 0
-        times = sql.get_player_best(mode=mode, guild_id=interaction.guild_id, player_id=player.id)
-        embed.title = f"{player.display_name} {speed.name} {items.name}"
-        embed.set_thumbnail(url=player.guild_avatar if player.guild_avatar else player.avatar)
-        if len(times) == 0:
-            embed.description = "No time to display sorry"
+    if track:
+        if player:
+            times = sql.get_user_track_time(mode=mode, guild_id=interaction.guild_id, track=track, player_id=player.id)
+            if len(times) == 0:
+                embed.title = f"{player.display_name} {track} {speed.name} {items.name}"
+                embed.description = "No time to display sorry"
+            else:
+                embed.title = f"{times[0][1]} {speed.name} {items.name}"
+                embed.set_thumbnail(url=times[0][3])
+                member = interaction.guild.get_member(times[0][0])
+                embed.description = f"{member.display_name} - `{times[0][2]}`"
+            
         else:
-            for time in times:
-                embed.description += f"**{time[0]}**: ``{time[1]}`` - {time[2]}/{time[3]}\n"
-                total += int(time[1].split(':')[0])*60*1000 + int(time[1].split(':')[1].split('.')[0])*1000 + int(time[1].split(':')[1].split('.')[1])
-            if len(times) == (len(sql.get_cups_emoji())*4):
-                embed.add_field(name="total time", value=format_time(total))
-
-    elif not player and not track:
-        times = sql.get_best_times(mode=mode, guild_id=interaction.guild_id)
-        embed.title = f"{speed.name} {items.name}"
-        embed.set_thumbnail(url=interaction.guild.icon)
-        if len(times) == 0:
-            embed.description = "No time to display sorry"
-        else:
-            for time in times:
-                member = interaction.guild.get_member(time[2])
-                embed.description += f"**{time[0]}**: ``{time[1]}`` - {member.display_name}\n"
-
-    elif not player and track:
-        times = sql.get_track_times(mode=mode, guild_id=interaction.guild_id, track=track)
-        if len(times) == 0:
-            embed.title = f"{track} {speed.name} {items.name}"
-            embed.description = "No time to display sorry"
-        else:
-            embed.title = f"{times[0][2]} {speed.name} {items.name}"
-            embed.set_thumbnail(url=times[0][3])
-            rank = 1
-            for time in times:
-                member = interaction.guild.get_member(time[0])
-                embed.description += f"**{rank}:** {member.display_name} ``{time[1]}``\n"
-                rank += 1
-
+            times = sql.get_track_times(mode=mode, guild_id=interaction.guild_id, track=track)
+            if len(times) == 0:
+                embed.title = f"{track} {speed.name} {items.name}"
+                embed.description = "No time to display sorry"
+            else:
+                embed.title = f"{times[0][2]} {speed.name} {items.name}"
+                embed.set_thumbnail(url=times[0][3])
+                rank = 1
+                for time in times:
+                    member = interaction.guild.get_member(time[0])
+                    embed.description += f"**{rank}:** {member.display_name} `{time[1]}`\n"
+                    rank += 1
     else:
-        times = sql.get_user_track_time(mode=mode, guild_id=interaction.guild_id, track=track, player_id=player.id)
-        if len(times) == 0:
-            embed.title = f"{player.display_name} {track} {speed.name} {items.name}"
-            embed.description = "No time to display sorry"
+        track_list_raw = sql.get_all_tracks()
+        fields = []
+        fields_title = ["__Nitro tracks__", "__Retro tracks__", "__DLC tracks__", "__Wave 1 & 2__", "__Wave 3 & 4__", "__Wave 5 & 6__"]
+
+        if player:
+            total = 0
+            times = sql.get_player_best(mode=mode, guild_id=interaction.guild_id, player_id=player.id)
+            embed.title = f"{player.display_name} {speed.name} {items.name}"
+            embed.set_thumbnail(url=player.display_avatar)
+            if len(times) == 0:
+                embed.description = "No time to display sorry"
+            else:
+                for index, track in enumerate(track_list_raw):
+                    if index%16==0:
+                        fields.append("")
+                    if len(times)>0 and track[0] == times[0][0]:
+                        time = times[0]
+                        fields[-1] += f"**{time[0]}**: `{time[1]}` - {time[2]}/{time[3]}\n"
+                        total += int(time[1].split(':')[0])*60*1000 + int(time[1].split(':')[1].split('.')[0])*1000 + int(time[1].split(':')[1].split('.')[1])
+                        times.pop(0)
+                for index, field in enumerate(fields):
+                    if len(field)>0:
+                        embed.add_field(name=f"{fields_title[index]}", value=field)
+
+                if len(times) == (len(sql.get_cups_emoji())*4):
+                    embed.add_field(name="total time", value=format_time(total))
+
         else:
-            embed.title = f"{times[0][1]} {speed.name} {items.name}"
-            embed.set_thumbnail(url=times[0][3])
-            member = interaction.guild.get_member(times[0][0])
-            embed.description = f"{member.display_name} - ``{times[0][2]}``"
+            times = sql.get_best_times(mode=mode, guild_id=interaction.guild_id)
+            embed.title = f"{speed.name} {items.name}"
+            embed.set_thumbnail(url=interaction.guild.icon)
+            if len(times) == 0:
+                embed.description = "No time to display sorry"
+            else:
+                for index, track in enumerate(track_list_raw):
+                    if index%16==0:
+                        fields.append("")
+                    if len(times)>0 and track[0] == times[0][0]:
+                        time = times[0]
+                        member = interaction.guild.get_member(time[2])
+                        fields[-1] += f"**{time[0]}**: `{time[1]}` - {member.display_name}\n"
+                        times.pop(0)
+                for index, field in enumerate(fields):
+                    if len(field)>0:
+                        embed.add_field(name=f"{fields_title[index]}", value=field)
+
 
     return await interaction.response.send_message(embed=embed)
 
