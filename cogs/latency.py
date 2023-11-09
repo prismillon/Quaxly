29b5dc449ce44688fe60ce6a65c4fcbc20defb4c
prsm <71454541+prismillon@users.noreply.github.com>
2023-11-09 19:41:14 +0100

    fix latency command

--- cogs/latency.py
+++ cogs/latency.py
@@ -1,8 +1,9 @@
 import discord
-import requests
+import aiohttp
 
 from discord.ext import commands
 from discord import app_commands
+from datetime import datetime
 
 class latency(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -15,9 +16,20 @@ class latency(commands.Cog):
         await interaction.response.defer()
 
         embed = discord.Embed(color=0x47e0ff, title="quaxly latency")
-        embed.add_field(name="discord API", value=f"{round(self.bot.latency, 3)}ms")
-        embed.add_field(name="lounge API", value=f"{round(requests.get('https://www.mk8dx-lounge.com/api/player').elapsed.total_seconds(), 3)}ms")
-        embed.add_field(name="mkc API", value=f"{round(requests.get('https://www.mariokartcentral.com/mkc/api/registry/players/1').elapsed.total_seconds(), 3)}ms")
+        embed.add_field(name="discord API", value=f"{round(self.bot.latency, 3)}s")
+        async with aiohttp.ClientSession() as session:
+            lounge_latency = datetime.now()
+            async with session.get('https://www.mk8dx-lounge.com/api/player?discordId=169497208406802432') as response:
+                if response.status == 200:
+                    embed.add_field(name="lounge API", value=f"{round((datetime.now() - lounge_latency).total_seconds(), 3)}s")
+                else:
+                    embed.add_field(name="lounge API", value="no response")
+            mkc_latency = datetime.now()
+            async with session.get('https://www.mariokartcentral.com/mkc/api/registry/players/1', ssl=False) as response:
+                if response.status == 200:
+                    embed.add_field(name="mkc API", value=f"{round((datetime.now() - mkc_latency).total_seconds(), 3)}s")
+                else:
+                    embed.add_field(name="mkc API", value="no response")
 
         await interaction.followup.send(embed=embed)
 
