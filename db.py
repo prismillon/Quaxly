4ec00d5e2a25f17451c077d6e7a1c470df1fb900
prsm <71454541+prismillon@users.noreply.github.com>
2024-04-12 20:55:00 +0200

    fix: redis connection

--- db.py
+++ db.py
@@ -1,6 +1,14 @@
+import re
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
+
+import redis.asyncio
+import redis
 
 mongodb_url = os.environ.get("MONGODB_URL")
 client = AsyncIOMotorClient(mongodb_url)
 db = client["quaxly"]
+
+redis_url = os.environ.get("REDIS_URL")
+r = redis.asyncio.Redis(host=redis_url, port=6379, socket_keepalive=True)
+rs = redis.Redis(host=redis_url, port=6379)
